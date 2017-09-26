030617e91608ba99a4745c42aafe369a1a5b5b88
Marie Morlet <marie.morlet@gmail.com>
2017-09-26 12:05:03 +0200

    Création du TeamDAO

---
+++ src/AppBundle/Service/DAO/TeamDAO.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marie
+ * Date: 26/09/2017
+ * Time: 11:56
+ */
+
+namespace AppBundle\Service\DAO;
+
+
+use AppBundle\Entity\Player;
+use AppBundle\Entity\Team;
+
+Class class TeamDAO {
+
+    public function getTeamById(int $id): Team {
+
+    }
+
+    public function saveTeam():Team {
+
+    }
+
+    public function getTeamWithPlayers(Player $player1, Player $player2): Team {
+
+    }
+
+
+}
