edd137dd0f007eac54ac98af6895f9a0f4b7c7f5
trigodetQuentin <trigodet.quentin@gmail.com>
2017-09-27 10:13:14 +0200

    Code + new

---
+++ src/AppBundle/Entity/TournamentType.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TournamentType
+ *
+ * @ORM\Table(name="rg_tournement_type")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TournamentTypeRepository")
+ */
+class TournamentType
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_tournament_type", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="number", type="integer")
+     */
+    private $number;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return TournamentType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     *
+     * @return TournamentType
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
+
