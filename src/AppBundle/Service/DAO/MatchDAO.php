2d1dc17d3141e4284647376a5c5d88f6120f94aa
Guillaume Orain <louveciennes.gui_appa@yahoo.fr>
2017-09-27 15:48:05 +0200

    Implémentation de la méthode pour rechercher les matchs disponibles pour les journalistes

--- src/AppBundle/Service/DAO/MatchDAO.php
+++ src/AppBundle/Service/DAO/MatchDAO.php
@@ -1,22 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AsusG74S
- * Date: 26/09/2017
- * Time: 12:05
- */
 
 namespace AppBundle\Service\DAO;
 
+use AppBundle\Entity\TennisMatch;
+use AppBundle\Repository\TennisMatchRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManager;
 
+/**
+ * Class MatchDAO
+ * @package AppBundle\Service\DAO
+ */
 class MatchDAO
 {
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
     public function saveMach(){
 
     }
 
-    public function getAllMatchs(){
+    /**
+     * @return Collection
+     * @throws \Exception
+     */
+    public function getAllMatchsScored()
+    {
+        // get repository
+        $repo = $this->getRepository();
 
+        try {
+            // get the query
+            $query = $repo->getAllMatchsScored();
+
+            return new ArrayCollection($query->getResult());
+        } catch (\Exception $exception) {
+            throw new \Exception(
+                "Une erreur est survenue lors de le recherche des matches.",
+                null,
+                $exception
+            );
+        }
     }
 
     public function getAllDuoMatchs(){
@@ -25,5 +57,13 @@ class MatchDAO
 
     public function getAllSimpleMarchs(){
 
+    }
+
+    /**
+     * @return TennisMatchRepository
+     */
+    private function getRepository()
+    {
+        return $this->em->getRepository(TennisMatch::class);
     }
 }
