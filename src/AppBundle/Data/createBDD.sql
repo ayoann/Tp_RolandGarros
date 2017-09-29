5a1b6f84c565207a347b3796f51ce1a0544d89f1
Guillaume Orain <louveciennes.gui_appa@yahoo.fr>
2017-09-29 09:09:59 +0200

    Humanisation des index + FK dans le script d'initialisation de la base de données

--- src/AppBundle/Data/createBDD.sql
+++ src/AppBundle/Data/createBDD.sql
@@ -13,7 +13,7 @@ CREATE TABLE rg_nationality (
   PRIMARY KEY(id_nationality)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
--- create table for entity Person with index on the id field
+-- create table for entity Person with index on the nationality id field
 CREATE TABLE rg_person (
   id_person INT AUTO_INCREMENT NOT NULL,
   nationality_id INT DEFAULT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE rg_person (
   lastname VARCHAR(255) NOT NULL,
   type VARCHAR(255) NOT NULL,
   female TINYINT(1) DEFAULT NULL,
-  INDEX IDX_B2F1BFD41C9DA55 (nationality_id),
+  INDEX INDEX_PLAYER_BY_NATIONALITY (nationality_id),
   PRIMARY KEY(id_person)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
@@ -31,8 +31,8 @@ CREATE TABLE rg_score (id_score INT AUTO_INCREMENT NOT NULL,
   match_id INT DEFAULT NULL,
   nb_set_win INT DEFAULT NULL,
   win TINYINT(1) NOT NULL,
-  INDEX IDX_CC254181296CD8AE (team_id),
-  INDEX IDX_CC2541812ABEACD6 (match_id),
+  INDEX INDEX_SCORE_BY_TEAM (team_id),
+  INDEX INDEX_SCORE_BY_MATCH (match_id),
   PRIMARY KEY(id_score)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
@@ -46,8 +46,8 @@ CREATE TABLE rg_team (
 CREATE TABLE rg_team_players (
   team_id INT NOT NULL,
   player_id INT NOT NULL,
-  INDEX IDX_1C015B89296CD8AE (team_id),
-  INDEX IDX_1C015B8999E6F5DF (player_id),
+  INDEX INDEX_TEAM_PLAYERS_BY_TEAM (team_id),
+  INDEX INDEX_TEAM_PLAYERS_BY_PLAYER (player_id),
   PRIMARY KEY(team_id, player_id)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
@@ -67,9 +67,9 @@ CREATE TABLE rg_tennis_match (
   tennis_court_id INT DEFAULT NULL,
   tennis_match_date DATETIME DEFAULT NULL,
   duration VARCHAR(5) DEFAULT NULL,
-  INDEX IDX_B026B86F33D1A3E7 (tournament_id),
-  INDEX IDX_B026B86F4A087CA2 (referee_id),
-  INDEX IDX_B026B86F7BCDB375 (tennis_court_id),
+  INDEX INDEX_MATCH_BY_TOURNAMENT (tournament_id),
+  INDEX INDEX_MATCH_BY_REFEREE (referee_id),
+  INDEX INDEX_MATCH_BY_TENNIS_COURT (tennis_court_id),
   PRIMARY KEY(id_tennis_match)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
@@ -81,7 +81,7 @@ CREATE TABLE rg_tournament (
   tournament_simple TINYINT(1) NOT NULL,
   class_name VARCHAR(15) NOT NULL,
   nb_set_max INT NOT NULL,
-  INDEX IDX_3C85F387AE437799 (tournament_type_id),
+  INDEX INDEX_TOURNAMENT_BY_TYPE (tournament_type_id),
   PRIMARY KEY(id_tournament)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
@@ -94,12 +94,12 @@ CREATE TABLE rg_tournament_type (
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 
 -- add foreign keys constraints
-ALTER TABLE rg_person ADD CONSTRAINT FK_B2F1BFD41C9DA55 FOREIGN KEY (nationality_id) REFERENCES rg_nationality (id_nationality);
-ALTER TABLE rg_score ADD CONSTRAINT FK_CC254181296CD8AE FOREIGN KEY (team_id) REFERENCES rg_team (id_team);
-ALTER TABLE rg_score ADD CONSTRAINT FK_CC2541812ABEACD6 FOREIGN KEY (match_id) REFERENCES rg_tennis_match (id_tennis_match);
-ALTER TABLE rg_team_players ADD CONSTRAINT FK_1C015B89296CD8AE FOREIGN KEY (team_id) REFERENCES rg_team (id_team);
-ALTER TABLE rg_team_players ADD CONSTRAINT FK_1C015B8999E6F5DF FOREIGN KEY (player_id) REFERENCES rg_person (id_person);
-ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_B026B86F33D1A3E7 FOREIGN KEY (tournament_id) REFERENCES rg_tournament (id_tournament);
-ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_B026B86F4A087CA2 FOREIGN KEY (referee_id) REFERENCES rg_person (id_person);
-ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_B026B86F7BCDB375 FOREIGN KEY (tennis_court_id) REFERENCES rg_tennis_court (id_tennis_court);
-ALTER TABLE rg_tournament ADD CONSTRAINT FK_3C85F387AE437799 FOREIGN KEY (tournament_type_id) REFERENCES rg_tournament_type (id_tournament_type);
+ALTER TABLE rg_person ADD CONSTRAINT FK_PLAYER_NATIONALITY FOREIGN KEY (nationality_id) REFERENCES rg_nationality (id_nationality);
+ALTER TABLE rg_score ADD CONSTRAINT FK_SCORE_TEAM FOREIGN KEY (team_id) REFERENCES rg_team (id_team);
+ALTER TABLE rg_score ADD CONSTRAINT FK_SCORE_MATCH FOREIGN KEY (match_id) REFERENCES rg_tennis_match (id_tennis_match);
+ALTER TABLE rg_team_players ADD CONSTRAINT FK_TEAM_PLAYERS_TEAM FOREIGN KEY (team_id) REFERENCES rg_team (id_team);
+ALTER TABLE rg_team_players ADD CONSTRAINT FK_TEAM_PLAYERS_PLAYER FOREIGN KEY (player_id) REFERENCES rg_person (id_person);
+ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_MATCH_TOURNAMENT FOREIGN KEY (tournament_id) REFERENCES rg_tournament (id_tournament);
+ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_MATCH_REFEREE FOREIGN KEY (referee_id) REFERENCES rg_person (id_person);
+ALTER TABLE rg_tennis_match ADD CONSTRAINT FK_MATCH_TENNIS_COURT FOREIGN KEY (tennis_court_id) REFERENCES rg_tennis_court (id_tennis_court);
+ALTER TABLE rg_tournament ADD CONSTRAINT FK_TOURNAMENT_TYPE FOREIGN KEY (tournament_type_id) REFERENCES rg_tournament_type (id_tournament_type);
